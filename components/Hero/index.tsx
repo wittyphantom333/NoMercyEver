d71c80b9b2264432a0e35805be6199be89434f72
Witt <adam.witt@iaawg.com>
2023-10-25 12:58:49 -0500

    updated frontpage

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,17 +15,17 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Free and Open-Source Next.js Template for Startup & SaaS
+                NoMercyEver Community Site
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                NoMercyEver is a group of knuckleheads with a dream... No one quite knows what that dream is, but here we are\!
+                NoMercyEver is a group of knuckleheads with a dream... No one quite knows what that dream is, but here we are!
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
                     href="https://discordapp.com/users/142034954262544384"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                   🔥 Get Pro
+                   Join Discord
                   </Link>
                   <Link
                     href="https://github.com/wittyphantom333/"
